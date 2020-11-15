5b798f61be71718dbf2b68f45a498914c6174483
iamarjun <arjunmanoj1995@gmail.com>
2020-11-15 22:46:07 +0530

    setup remote entity class which fetches data from the firebase firestore

---
+++ app/src/main/java/com/arjun/streamy/data/Constants.kt
@@ -0,0 +1,6 @@
+package com.arjun.streamy.data
+
+object Constants {
+
+    const val SONG_COLLECTION = "songs"
+}
---
+++ app/src/main/java/com/arjun/streamy/data/remote/MusicDatabase.kt
@@ -0,0 +1,21 @@
+package com.arjun.streamy.data.remote
+
+import com.arjun.streamy.data.Constants.SONG_COLLECTION
+import com.arjun.streamy.data.entities.Song
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
+object MusicDatabase {
+
+    private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private val songCollection = firestore.collection(SONG_COLLECTION)
+
+    suspend fun getAllSongs(): List<Song> {
+        return try {
+            songCollection.get().await().toObjects(Song::class.java)
+        } catch (e: Exception) {
+            emptyList()
+        }
+    }
+
+}
